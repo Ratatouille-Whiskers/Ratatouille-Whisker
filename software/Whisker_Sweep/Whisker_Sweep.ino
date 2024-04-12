97ddee097d0ef57228934cbc7962e6cd4842595d
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2024-04-12 16:30:32 +0100

    Added Interrupt pin signal that when triggered with reverse the servo dir.
    This is so that we can sweep the servo but reverse it if we find that the whisker has detected an obsticale and we want to signal a stop to prevent damaging the whiskers by moving in the opposite direction at a faster rate before resetting back to default sweep speed

--- software/Whisker_Sweep/Whisker_Sweep.ino
+++ software/Whisker_Sweep/Whisker_Sweep.ino
@@ -1,25 +1,61 @@
-s#include <Servo.h>
+#include <Servo.h>
 
-Servo servo;  // create servo object
+#define SERVO_PIN 16
+#define REVERSE_INTERRUPT_PIN 17
+#define DEFAULT_SWEEP_DELAY 15
+#define FAST_SWEEP_DELAY 2
 
-void setup() {
-  // our MG966 (clone) servo has a pulse width responce range of approximately 500-3000ms
-  servo.attach(16, 500, 3000);  // attaches the servo on GIO2 to the servo object
+Servo servo; // create servo object
+bool reverse = false;
+int sweepDelay = DEFAULT_SWEEP_DELAY; // use to control the speed of the sweep
+
+// interrupt that will reverse the direction of the sweep when triggered so that we
+// avoid damaging the whisker
+void reverseSweepInterrupt()
+{
+  reverse = true;
 }
 
-void loop() {
+void setup()
+{
+  // our MG966 (clone) servo has a pulse width responce range of approximately 500-3000ms
+  servo.attach(SERVO_PIN, 500, 3000); // attaches the servo on GIO2 to the servo object
+
+  pinMode(REVERSE_INTERRUPT_PIN, INPUT_PULLDOWN);
+  attachInterrupt(REVERSE_INTERRUPT_PIN, reverseSweepInterrupt, RISING);
+}
+
+void loop()
+{
   int pos;
+  // reset the sweep delay to the default in case we had to reverse the sweep direction to avoid damaging the whisker as faster rate
+  sweepDelay = DEFAULT_SWEEP_DELAY;
 
   // Direction A
-  for (pos = 0; pos <= 180; pos += 1) {
-    servo.write(pos);              
-    delay(15);
+  for (pos = 0; pos <= 180; pos += 1)
+  {
+    // check if interrupt has been triggered, and reverse direction if it has
+    if (reverse)
+    {
+      reverse = false;
+      sweepDelay = FAST_SWEEP_DELAY;
+      break;
+    }
+    servo.write(pos);
+    delay(sweepDelay);
   }
 
   // Direction B
-  for (pos = 180; pos >= 0; pos -= 1) { 
-    servo.write(pos);              
-    delay(15);                     
+  for (pos = 180; pos >= 0; pos -= 1)
+  {
+    // check if interrupt has been triggered, and reverse direction if it has
+    if (reverse)
+    {
+      reverse = false;
+      sweepDelay = FAST_SWEEP_DELAY;
+      break;
+    }
+    servo.write(pos);
+    delay(sweepDelay);
   }
 }
-
